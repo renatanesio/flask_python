94a6582cd4eb5cdb8a2fbb7686accc9ef0a76753
Renata Nésio <renata.nesio@mail.ru>
2020-12-30 13:31:04 -0300

    Added database exercise

---
+++ SQL Databases with Flask/lesson_90_exercise/forms.py
@@ -0,0 +1,22 @@
+# forms.py
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+
+
+class AddForm(FlaskForm):
+
+    name = StringField('Name of Puppy: ')
+    submit = SubmitField('Add Puppy')
+
+
+class DelForm(FlaskForm):
+
+    id = IntegerField('Id Number of Puppy to Remove: ')
+    submit = SubmitField('Remove Puppy')
+
+
+class AddOwnerForm(FlaskForm):
+
+    name = StringField('Name of Owner: ')
+    pup_id = StringField('Id of Puppy: ')
+    submit = SubmitField('Add Owner')
