d50996db662e271114c0138f50c9b56bb69cf740
Renata Nésio <renata.nesio@mail.ru>
2020-12-30 09:21:13 -0300

    Initial commit

---
+++ SQL Databases with Flask/lesson_82_databases/crud.py
@@ -0,0 +1,36 @@
+from basic import db, Puppy
+
+# CREATE #
+
+my_puppy = Puppy('Rufus', 5)
+db.session.add(my_puppy)
+db.session.commit()
+
+# READ #
+all_puppies = Puppy.query.all()
+print(all_puppies)
+
+# Select by ID #
+puppy_one = Puppy.query.get(1)
+print(puppy_one.name)
+
+# FILTERS #
+puppy_frankie = Puppy.query.filter_by(name='Frankie')
+print(puppy_frankie.all())
+
+
+# UPDATE #
+first_puppy = Puppy.query.get(1)
+first_puppy.age = 10
+db.session.add(first_puppy)
+db.session.commit()
+
+# DELETE #
+second_pup = Puppy.query.get(2)
+db.session.delete(second_pup)
+db.session.commit()
+
+#
+
+all_puppies = Puppy.query.all()
+print(all_puppies)
